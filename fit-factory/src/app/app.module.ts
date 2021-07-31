1886c90ad159b2b2d5c8aaa9c49b5635a3d4a3d0
Valionas <sdms@abv.bg>
2021-07-31 11:42:08 +0300

    Material UI + First Components

--- fit-factory/src/app/app.module.ts
+++ fit-factory/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { LoginComponent } from './auth-components/login/login.component';
+import { RegisterComponent } from './auth-components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,  
+    MatSliderModule,  
   ],
   providers: [],
   bootstrap: [AppComponent]
