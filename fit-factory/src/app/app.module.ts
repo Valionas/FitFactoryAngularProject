05d0fdf9c200d7bbaae350c939e8a0bac14b2ce3
Valionas <sdms@abv.bg>
2021-08-01 09:55:30 +0300

    FireBase Config

--- fit-factory/src/app/app.module.ts
+++ fit-factory/src/app/app.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//Firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorage, AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuth, AngularFireAuthModule } from '@angular/fire/auth';
+//Material UI
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+//App Components
 import { LoginComponent } from './auth-components/login/login.component';
 import { RegisterComponent } from './auth-components/register/register.component';
 import { PersonalCalorieCalculatorComponent } from './personal-calorie-calculator/personal-calorie-calculator.component';
 import { WorkoutsComponent } from './workouts/workouts.component';
 import { DietsComponent } from './diets/diets.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent, 
     LoginComponent,
     RegisterComponent,
     PersonalCalorieCalculatorComponent,
@@ -23,8 +30,12 @@ import { DietsComponent } from './diets/diets.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,  
-    MatSliderModule,  
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- fit-factory/src/environments/environment.ts
+++ fit-factory/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA3fr7lwJYDnW6iSD4804v4mh7Y8mHeC0M",
+    authDomain: "fit-factory-1d3d4.firebaseapp.com",
+    projectId: "fit-factory-1d3d4",
+    storageBucket: "fit-factory-1d3d4.appspot.com",
+    messagingSenderId: "519492857282",
+    appId: "1:519492857282:web:b3eeb39fc92825a882fa5b"
+  }
 };
 
 /*
