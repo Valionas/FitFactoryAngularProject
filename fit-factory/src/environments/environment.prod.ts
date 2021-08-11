f9481acfd027faa0c6c45b06ec686ae7ddb8f18d
Valentin Kolev <sdms@abv.bg>
2021-08-11 16:10:52 +0300

    Animate CSS optimization

--- fit-factory/src/environments/environment.prod.ts
+++ fit-factory/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA3fr7lwJYDnW6iSD4804v4mh7Y8mHeC0M",
+    authDomain: "fit-factory-1d3d4.firebaseapp.com",
+    projectId: "fit-factory-1d3d4",
+    storageBucket: "fit-factory-1d3d4.appspot.com",
+    messagingSenderId: "519492857282",
+    appId: "1:519492857282:web:b3eeb39fc92825a882fa5b"
+  }
 };
